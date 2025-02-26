c4f1692f1dcd794eb1f813b76be72d520c6e22b1
Hugh Graham <hugh.a.graham@gmail.com>
2025-02-26 00:47:24 +0000

    set gdal options on start up

--- R/package.R
+++ R/package.R
@@ -17,6 +17,7 @@
 .onLoad <- function(libname, pkgname) {
   python_init_checks()
   cache_init_checks()
+  gdal_init_opts()
 }
 
 python_init_checks <- function() {
@@ -49,4 +50,40 @@ cache_init_checks <- function() {
 
   toset <- !(names(op_vrtility) %in% names(op))
   if (any(toset)) options(op_vrtility[toset])
+}
+
+gdal_init_opts <- function() {
+  gdal_config_opts <- c(
+    GDAL_VRT_ENABLE_PYTHON = "YES",
+    VSI_CACHE = "TRUE",
+    GDAL_CACHEMAX = "30%",
+    VSI_CACHE_SIZE = "10000000",
+    GDAL_HTTP_MULTIPLEX = "YES",
+    GDAL_INGESTED_BYTES_AT_OPEN = "32000",
+    GDAL_DISABLE_READDIR_ON_OPEN = "EMPTY_DIR",
+    GDAL_HTTP_VERSION = "2",
+    GDAL_HTTP_MERGE_CONSECUTIVE_RANGES = "YES",
+    GDAL_NUM_THREADS = "ALL_CPUS"
+    # , CPL_DEBUG = "ON"
+  )
+
+  gdal_warp_opts <- c(
+    "-r",
+    "bilinear",
+    "-co",
+    "COMPRESS=DEFLATE",
+    "-co",
+    "PREDICTOR=2",
+    "-co",
+    "NUM_THREADS=ALL_CPUS"
+  )
+
+  op <- options()
+  op_gdal <- list(
+    vrt.gdal.config.options = gdal_config_opts,
+    vrt.gdal.warp.options = gdal_warp_opts
+  )
+
+  toset <- !(names(op_gdal) %in% names(op))
+  if (any(toset)) options(op_gdal[toset])
 }
