eccacfd40e996c80dd3d68535b713ad61524bd19
Hugh Graham <hugh.a.graham@gmail.com>
2025-02-26 00:44:24 +0000

    new vrt helpers

--- R/vrt-funs.R
+++ R/vrt-funs.R
@@ -8,12 +8,11 @@
 #' This function will add a pixel function to every band in a VRT file. Then,
 #' when passed to gdal warp or translate the pixel function will be applied.
 vrt_add_pixfun <- function(
-    src,
-    vrt_opts = NULL, pix_fun = vrtility::numba_median) {
-  v_assert_type(src, "src",
-    c("character", "stac_vrt"),
-    multiple = TRUE
-  )
+  src,
+  vrt_opts = NULL,
+  pix_fun = vrtility::numba_median
+) {
+  v_assert_type(src, "src", c("character", "stac_vrt"), multiple = TRUE)
   v_assert_type(pix_fun, "fun", "function")
 
   if (rlang::inherits_all(src, "stac_vrt")) {
@@ -44,7 +43,6 @@ vrt_add_pixfun <- function(
     xml2::xml_add_child(pixel_func_code, cdata_node)
   })
 
-
   return(build_stac_vrt(
     xml = tvrt,
     bbox = src$bbox,
@@ -54,4 +52,51 @@ vrt_add_pixfun <- function(
     assets = band_names,
     pixfun = pix_fun()
   ))
+}
+
+#' Set the descriptions for the bands in a VRT
+#' @param x A character string of the path to the VRT
+#' @param descriptions A character vector of the descriptions
+#' @param as_file A logical indicating if the VRT should be saved to a file
+#' @return A character string of the path to the modified VRT
+#' @keywords internal
+#' @noRd
+set_vrt_descriptions <- function(x, descriptions, as_file = FALSE) {
+  mvrt <- xml2::read_xml(x)
+  mvrt_bands <- xml2::xml_find_all(mvrt, "//VRTRasterBand")
+  purrr::walk2(
+    mvrt_bands,
+    descriptions,
+    function(x, y) {
+      xml2::xml_set_attr(x, "Description", y)
+    }
+  )
+
+  if (as_file) {
+    xml2::write_xml(mvrt, x)
+    return(x)
+  } else {
+    return(mvrt)
+  }
+}
+
+set_vrt_metadata <- function(x, keys, values, as_file = FALSE) {
+  v_assert_length(keys, "keys", length(values))
+  mvrt <- xml2::read_xml(x)
+
+  metadchild <- xml2::xml_add_child(mvrt, "Metadata")
+  purrr::walk2(
+    keys,
+    values,
+    function(x, y) {
+      mdi <- xml2::xml_add_child(metadchild, "MDI", y)
+      xml2::xml_set_attr(mdi, "key", x)
+    }
+  )
+  if (as_file) {
+    xml2::write_xml(mvrt, x)
+    return(x)
+  } else {
+    return(mvrt)
+  }
 }
