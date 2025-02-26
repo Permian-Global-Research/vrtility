49bb8ed9583fccff508ba900c1d9f5854772a6f3
Hugh Graham <hugh.a.graham@gmail.com>
2025-02-26 00:44:59 +0000

    move all the print msgs

--- R/ui-utils.R
+++ R/ui-utils.R
@@ -10,11 +10,12 @@
 #' @noRd
 #' @details by hadely: https://github.com/r-lib/cli/issues/228
 nice_menu <- function(
-    prompt,
-    choices,
-    not_interactive = "This function requires an interactive session",
-    quit = integer(),
-    .envir = rlang::caller_env()) {
+  prompt,
+  choices,
+  not_interactive = "This function requires an interactive session",
+  quit = integer(),
+  .envir = rlang::caller_env()
+) {
   if (!rlang::is_interactive()) {
     cli::cli_abort(not_interactive, .envir = .envir)
   }
@@ -42,4 +43,127 @@ nice_menu <- function(
     cli::cli_abort("Quiting...", call = NULL)
   }
   selected
+}
+
+
+# printing helpers
+
+vrt_print_msg <- function() {
+  cli::cli_inform(
+    c(
+      paste(cli::style_bold(
+        cli::col_yellow("\n VRT XML: "),
+        "[hidden]"
+      )),
+      " " = cli::style_italic(
+        "run {cli::code_highlight('print(x, xml = TRUE)')} to view"
+      )
+    )
+  )
+}
+
+pixel_fun_printer <- function(x) {
+  cli::cli_inform(
+    paste(cli::style_bold(cli::col_blue("Pixel Function:"))),
+  )
+
+  cli::code_highlight(
+    cli::cli_code(format(x), language = "python")
+  )
+}
+
+pix_fun_print_msg <- function() {
+  cli::cli_inform(
+    c(
+      paste(cli::style_bold(
+        cli::col_yellow("Pixel Function: "),
+        "[hidden]"
+      )),
+      " " = cli::style_italic(
+        "run {cli::code_highlight('print(x, pixfun = TRUE)')} to view"
+      )
+    )
+  )
+}
+
+bbox_printer <- function(x) {
+  formatted_bbox <- format(x, scientific = FALSE, trim = TRUE)
+
+  paste(
+    cli::style_bold(cli::col_blue("Bounding Box:")),
+    paste(formatted_bbox, collapse = " ")
+  )
+}
+
+dttm_printer <- function(x, type = c("dttm", "start", "end")) {
+  dt_lab <- switch(
+    type,
+    dttm = "Date Time",
+    start = "Start Date",
+    end = "End Date"
+  )
+
+  paste(
+    cli::style_bold(cli::col_blue(
+      glue::glue(dt_lab),
+      ":"
+    )),
+
+    if (inherits(x, "POSIXct")) {
+      x
+    } else {
+      lubridate::as_datetime(x)
+    }
+  )
+}
+
+n_items_printer <- function(x) {
+  paste(
+    cli::style_bold(cli::col_blue("Number of Items:")),
+    x
+  )
+}
+
+assets_printer <- function(x) {
+  paste(
+    cli::style_bold(cli::col_blue("Assets:")),
+    paste(x, collapse = ", ")
+  )
+}
+
+no_data_printer <- function(x) {
+  paste(
+    cli::style_bold(cli::col_blue("No Data Value(s):")),
+    paste(x, collapse = ", ")
+  )
+}
+
+res_printer <- function(x) {
+  paste(
+    cli::style_bold(cli::col_blue("Pixel res:")),
+    paste(x, collapse = ", ")
+  )
+}
+
+
+xml_printer <- function(x) {
+  cli::style_bold("VRT XML: \n\n") |>
+    cli::col_yellow() |>
+    cat()
+  paste0(x, "\n\n") |>
+    cli::style_italic() |>
+    cli::col_green() |>
+    cat()
+}
+
+
+crs_printer <- function(x) {
+  #TODO: add srs_find_epsg and friends when I get the dev gdalraster installed.
+  cli::style_bold("\n VRT SRS: \n") |>
+    cli::col_yellow() |>
+    cat()
+  paste0(x, "\n", sep = "\n") |>
+    cli::style_italic() |>
+    cli::col_br_magenta() |>
+    cat()
 }
