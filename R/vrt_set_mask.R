935bdcd72d189edca5dec2e9f0a71e793e29df46
Hugh Graham <hugh.a.graham@gmail.com>
2025-02-27 19:48:09 +0000

    seperate mask pixel functions from calling function

--- R/vrt_set_mask.R
+++ R/vrt_set_mask.R
@@ -14,9 +14,24 @@ vrt_set_mask.default <- function(x, ...) {
   )
 }
 
+#' @param x A VRT block
+#' @param mask_band The name of the mask band
+#' @param valid_bits A numeric vector of valid bits
+#' @param mask_pix_fun A function that returns the Python code for the mask
+#' pixel function
+#' @return A VRT block with the mask band set.
 #' @export
 #' @rdname vrt_set_mask
-vrt_set_mask.vrt_block <- function(x, mask_band) {
+vrt_set_mask.vrt_block <- function(
+  x,
+  mask_band,
+  valid_bits,
+  mask_pix_fun = vrtility::bitmask_numba
+) {
+  v_assert_type(mask_band, "mask_band", "character", nullok = FALSE)
+  v_assert_type(valid_bits, "valid_bits", "numeric", nullok = FALSE)
+  v_assert_type(mask_pix_fun, "mask_pix_fun", "function", nullok = FALSE)
+
   vx <- xml2::read_xml(x$vrt)
 
   bands <- xml2::xml_find_all(vx, ".//VRTRasterBand")
@@ -35,71 +50,48 @@ vrt_set_mask.vrt_block <- function(x, mask_band) {
     ))
   }
 
-  mask_band_node <- bands[[mask_idx]]
-
-  # Set pixel function attributes
-  xml2::xml_set_attr(mask_band_node, "subClass", "VRTDerivedRasterBand")
-  xml2::xml_add_child(mask_band_node, "PixelFunctionType", "sentinel2_mask")
-  xml2::xml_add_child(mask_band_node, "PixelFunctionLanguage", "Python")
-
-  # Add pixel function code that handles SCL values
-  pixel_func <- glue::glue(
-    "
-import numpy as np
-breakpoint()
-def sentinel2_mask(in_ar, out_ar, xoff, yoff, xsize, ysize, raster_xsize,
-                  raster_ysize, buf_radius, gt, **kwargs):
-    # Valid SCL values:
-    # 4 = Vegetation
-    # 5 = Not vegetated
-    # 6 = Water
-    # 7 = Unclassified
-    # 11 = Snow
-    valid_values = [4, 5, 6, 7, 11]
-    
-    breakpoint()
-    # Set mask to 1 (valid) for valid pixels, 0 (invalid) for others
-    mask = np.isin(in_ar[0], valid_values)
-    out_ar[:] = mask.astype(np.uint8)
-"
+  mask_src <- xml2::xml_find_all(
+    bands[[mask_idx]],
+    ".//SimpleSource | .//ComplexSource"
   )
+  mask_src_chr <- xml2::read_xml(as.character(mask_src))
 
-  # Add pixel function code as CDATA
-  cdata_node <- xml2::xml_cdata(pixel_func)
-  pixel_func_code <- xml2::xml_add_child(mask_band_node, "PixelFunctionCode")
-  xml2::xml_add_child(pixel_func_code, cdata_node)
+  purrr::walk(bands[-mask_idx], function(.x) {
+    xml2::xml_add_child(.x, mask_src_chr)
 
-  # browser()
-  # cat(as.character(vx))
+    xml2::xml_set_attr(.x, "subClass", "VRTDerivedRasterBand")
+    xml2::xml_add_child(.x, "PixelFunctionType", "sentinel2_mask")
+    xml2::xml_add_child(.x, "PixelFunctionLanguage", "Python")
+    pf_args <- xml2::xml_add_child(.x, "PixelFunctionArguments")
+    xml2::xml_set_attr(
+      pf_args,
+      "valid_values",
+      paste(valid_bits, collapse = ",")
+    )
 
-  # Remove the mask band from its current position
-  # Create MaskBand element and add the VRTRasterBand to it
-
-  ndv <- xml2::xml_find_all(mask_band_node, "NoDataValue")
-  xml2::xml_remove(ndv)
-  masksrc <- xml2::xml_find_all(
-    mask_band_node,
-    "//SimpleSource | //ComplexSource"
-  )
-  xml2::xml_set_name(masksrc, "SimpleSource")
-  ndc <- xml2::xml_find_all(masksrc, "NODATA")
-  xml2::xml_remove(ndc)
-  xml2::xml_remove(mask_band_node)
-
-  main_bands <- xml2::xml_find_all(
-    vx,
-    "//SimpleSource | //ComplexSource"
-  )
-  purrr::walk(main_bands, function(x) {
-    nodatachild <- xml2::xml_find_all(x, "NODATA")
-    xml2::xml_remove(nodatachild)
-    use_mask <- xml2::xml_add_child(x, "UseMaskBand")
-    xml2::xml_text(use_mask) <- "true"
+    cdata_node <- xml2::xml_cdata(mask_pix_fun())
+    pixel_func_code <- xml2::xml_add_child(.x, "PixelFunctionCode")
+    xml2::xml_add_child(pixel_func_code, cdata_node)
   })
-  mask_band_xml <- xml2::xml_add_child(vx, "MaskBand")
-  xml2::xml_add_child(mask_band_xml, mask_band_node)
 
   # Write back to block
-  x$vrt <- as.character(vx)
-  return(x)
+  tf <- fs::file_temp(tmp_dir = getOption("vrt.cache"), ext = "vrt")
+  xml2::write_xml(vx, tf)
+  build_vrt_block(tf, maskfun = mask_pix_fun(), pixfun = x$pixfun)
+}
+
+#' @param x A VRT collection
+#' @rdname vrt_set_mask
+#' @export
+#' @return A VRT collection with the mask band set.
+#' @export
+vrt_set_mask.vrt_collection <- function(
+  x,
+  mask_band,
+  valid_bits,
+  mask_pix_fun = vrtility::bitmask_numba
+) {
+  # browser()
+  purrr::map(x$vrt, ~ vrt_set_mask(.x, mask_band, valid_bits, mask_pix_fun)) |>
+    build_vrt_collection(maskfun = mask_pix_fun(), pixfun = x$pixfun)
 }
