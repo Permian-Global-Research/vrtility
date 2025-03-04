3c6baefe9bc6724af868e859db35453a08fb69d9
Hugh Graham <hugh.a.graham@gmail.com>
2025-03-04 15:39:14 +0000

    automated scaling in plotting

--- R/plot-raster-src.R
+++ R/plot-raster-src.R
@@ -10,16 +10,13 @@
 plot_raster_src <- function(
   x,
   bands = 1,
-  pal = grDevices::hcl.colors(10, "viridis"),
-  xsize = NULL,
-  ysize = NULL,
+  pal = grDevices::hcl.colors(10, "Viridis"),
   nbands = length(bands),
-  max_pixels = 2.5e+07,
   col_tbl = NULL,
   maxColorValue = 1,
   normalize = TRUE,
   minmax_def = NULL,
-  minmax_pct_cut = if (length(bands) == 3) c(1, 97) else NULL,
+  minmax_pct_cut = if (length(bands) == 3) c(1, 95) else NULL,
   col_map_fn = if (nbands == 1) scales::colour_ramp(pal, alpha = FALSE) else
     NULL,
   xlim = NULL,
@@ -28,27 +25,39 @@ plot_raster_src <- function(
   asp = 1,
   axes = TRUE,
   main = "",
-  xlab = "x",
-  ylab = "y",
-  xaxs = "i",
-  yaxs = "i",
+  xlab = "",
+  ylab = "",
+  xaxs = "r",
+  yaxs = "r",
   legend = if (nbands == 1) TRUE else FALSE,
   digits = 2,
   na_col = rgb(0, 0, 0, 0),
   ...
 ) {
-  ds <- new(gdalraster::GDALRaster, x)
+  dpi <- dev.size("px")[1] / dev.size("in")[1]
+  dev_inches <- graphics::par("din") # Returns c(width, height) in inches
+  dev_size <- dev_inches * dpi
+  target_divisor <- dev_size[1] * 1.5
 
-  r <- gdalraster::read_ds(ds, bands = bands)
+  ds <- new(gdalraster::GDALRaster, x)
+  r <- gdalraster::read_ds(
+    ds,
+    bands = bands,
+    out_xsize = ceiling(
+      ds$getRasterXSize() /
+        ceiling(ds$getRasterXSize() / target_divisor)
+    ),
+    out_ysize = ceiling(
+      ds$getRasterYSize() /
+        ceiling(ds$getRasterYSize() / target_divisor)
+    )
+  )
 
   ds$close()
 
   gdalraster::plot_raster(
     r,
-    xsize = xsize,
-    ysize = ysize,
     nbands = nbands,
-    max_pixels = max_pixels,
     col_tbl = col_tbl,
     maxColorValue = maxColorValue,
     normalize = normalize,
