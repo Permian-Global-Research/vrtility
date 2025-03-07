329972695673b8146541b16b8c825697a67c2733
Hugh Graham <hugh.a.graham@gmail.com>
2025-03-07 22:59:27 +0000

    remove build bit mask numba function

--- R/mask-pixel-funs.R
+++ R/mask-pixel-funs.R
@@ -53,7 +53,6 @@ def bitmask(in_ar, out_ar, xoff, yoff, xsize, ysize, raster_xsize,
 
 
 #' Masking functions for pixel-based processing
-#' @export
 #' @details This function is a simple bit mask function that can be used to
 #' mask out pixels based on a bit mask. There is no bitwise transformation
 #' applied to the provided valid_bits.
@@ -63,48 +62,13 @@ build_bitmask_numpy <- function() {
   glue::glue(
     "
 import numpy as np
-def bitmask(in_ar, out_ar, xoff, yoff, xsize, ysize, raster_xsize,
+def build_bitmask(in_ar, out_ar, xoff, yoff, xsize, ysize, raster_xsize,
                   raster_ysize, buf_radius, gt, **kwargs):
     valid_vals =  [int(x) for x in kwargs['valid_values'].decode().split(',')]
 
     mask = np.isin(in_ar[0], valid_vals)
     # breakpoint()
     out_ar[:] = np.where(mask, 255, 0)  # Set invalid pixels to 0
-"
-  )
-}
-
-
-build_bitmask_numba <- function() {
-  glue::glue(
-    "
-import numpy as np
-import numba
-from numba import prange
-
-@numba.jit(nopython=True, parallel=True)
-def create_mask(data, valid_vals):
-    rows, cols = data.shape
-    out = np.zeros((rows, cols), dtype=np.uint8)
-    
-    # Use prange for outer loop
-    for i in prange(rows):
-        for j in range(cols):
-            # Check if pixel value is in valid_vals
-            pixel_val = data[i, j]
-            for val in valid_vals:
-                if pixel_val == val:
-                    out[i, j] = 255  # Valid pixel
-                    break
-    return out
-
-def bitmask(in_ar, out_ar, xoff, yoff, xsize, ysize, raster_xsize,
-            raster_ysize, buf_radius, gt, **kwargs):
-    valid_vals = np.array([int(x) for x in kwargs['valid_values'].decode().split(',')], 
-                         dtype=np.int32)
-    
-    # Create mask using numba-optimized function
-    out_ar[:] = create_mask(in_ar[0], valid_vals)
 "
   )
 }
