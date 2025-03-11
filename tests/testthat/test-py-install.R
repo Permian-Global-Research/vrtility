3113956041204f4342e874111ec683d3f273a9bd
Hugh Graham <hugh.a.graham@gmail.com>
2025-03-11 15:39:21 +0000

    test python env things

---
+++ tests/testthat/test-py-install.R
@@ -0,0 +1,16 @@
+test_that("python can be installed", {
+  expect_warning(
+    vrtility_py_require("numpy", "numba")
+  )
+})
+
+test_that("python env can be located", {
+  expect_true(fs::file_exists(Sys.getenv("VRTILITY_PY_EXECUTABLE")))
+  expect_gte(as.numeric(Sys.getenv("VRTILITY_PY_VERSION_MAJOR")), 3)
+  expect_gte(as.numeric(Sys.getenv("VRTILITY_PY_VERSION_MINOR")), 0)
+})
+
+test_that("python version parses correctly", {
+  expect_type(as.numeric(Sys.getenv("VRTILITY_PY_VERSION_MINOR")), "double")
+  expect_false(is.na(as.numeric(Sys.getenv("VRTILITY_PY_VERSION_MINOR"))))
+})
