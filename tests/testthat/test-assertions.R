88795cb332924570531892642b6fb2f0fc875f2e
Hugh Graham <hugh.a.graham@gmail.com>
2025-03-21 21:32:38 +0000

    add more tests

---
+++ tests/testthat/test-assertions.R
@@ -0,0 +1,22 @@
+test_that("assertions work", {
+  s2files <- fs::dir_ls(system.file("s2-data", package = "vrtility"))
+  ex_collect <- vrt_collect(s2files)
+  test_xml <- xml2::read_xml(as.character(ex_collect[[1]][[1]]$vrt))
+  band1 <- xml2::xml_find_first(test_xml, ".//.//VRTRasterBand")
+  xml2::xml_add_child(band1, "SillyBilly", "wolly")
+  tvrt <- fs::file_temp(ext = "vrt")
+  xml2::write_xml(test_xml, tvrt)
+  expect_error(v_assert_valid_schema(tvrt))
+
+  expect_error(v_assert_true(FALSE, "testy"))
+
+  expect_equal(v_assert_res(c(2, 2)), c(2, 2))
+  expect_equal(v_assert_res(2), c(2, 2))
+  expect_error(v_assert_res(c(2, 2, 2)))
+
+  # tests assert_srs_len - vrt_stack should fail with multple srs in collection
+  expect_error(vrt_stack(ex_collect))
+
+  # tests assert_files_exist - vrt_collect should fail with non-existent files
+  expect_error(vrt_collect(c("notmyfile.tif", "northisone.tif")))
+})
---
+++ tests/testthat/test-stac-utils.R
@@ -0,0 +1,22 @@
+test_that("hls_stac_query works", {
+  skip_if_offline()
+  skip_on_cran()
+  bbox <- gdalraster::bbox_from_wkt(
+    wkt = wk::wkt("POINT (144.3 -7.6)"),
+    extend_x = 0.17,
+    extend_y = 0.125
+  )
+  te <- bbox_to_projected(bbox)
+  trs <- attr(te, "wkt")
+
+  hls_query <- hls_stac_query(
+    bbox,
+    start_date = "2023-01-01",
+    end_date = "2023-02-28",
+    assets = c("B04", "B03", "B02", "Fmask"),
+    collection = "HLSS30_2.0",
+    max_cloud_cover = 70
+  )
+  expect_s3_class(hls_query, "doc_items")
+  expect_equal(length(hls_query$features), 5)
+})
