b57638641518fc70ff53eea99532d8ba3611863a
Hugh Graham <hugh.a.graham@gmail.com>
2025-04-13 02:24:13 +0100

    WIP tile aggregation function...

--- R/tiling.R
+++ R/tiling.R
@@ -69,3 +69,78 @@ get_tiles <- function(img_rows, img_cols, x_window, y_window, overlap = 0) {
 
   return(mat)
 }
+
+
+#' Aggregate raster blocks
+#'
+#' @param blocks_df A data frame of block definitions with columns nXOff, nYOff, nXSize, nYSize
+#' @param max_size Maximum size in pixels for aggregated blocks (default 1024)
+#' @param direction Direction of aggregation ("row" or "both")
+#' @return A data frame of aggregated blocks
+#' @examples
+#' blocks_df <- get_tiles(1000, 1000, 128, 128)
+#' # Aggregate blocks in rows
+#' aggregate_blocks(blocks_df, max_size = 512)
+#' @export
+aggregate_blocks <- function(
+  blocks_df,
+  max_size = 1024,
+  direction = "row",
+  debug = FALSE
+) {
+  # Sort blocks by row (nYOff) then column (nXOff)
+  blocks_df <- blocks_df[order(blocks_df$nYOff, blocks_df$nXOff), ]
+
+  # Split blocks by row (same nYOff)
+
+  row_groups <- split(blocks_df, blocks_df$nYOff)
+  # browser()
+  if (debug) browser()
+  if (all(lapply(row_groups, nrow) == 1)) {
+    row_groups <- list(blocks_df)
+  }
+
+  if (direction == "row") {
+    adj_col <- "nXSize"
+    alt_col <- "nYSize"
+  } else {
+    adj_col <- "nYSize"
+    alt_col <- "nXSize"
+  }
+
+  # Aggregate blocks within each row
+  aggregated_blocks <- do.call(
+    rbind,
+    lapply(row_groups, function(row_blocks) {
+      # Initialize result for this row
+      # browser()
+      result_blocks <- data.frame()
+      current_block <- row_blocks[1, ]
+      current_size <- current_block[adj_col]
+      ncell <- current_size * current_block[alt_col]
+
+      for (i in 2:nrow(row_blocks)) {
+        if (
+          ncell + (row_blocks[i, adj_col] * row_blocks[i, alt_col]) <= max_size
+        ) {
+          # Merge with current block
+          current_block[adj_col] <- current_block[adj_col] +
+            row_blocks[i, adj_col]
+          current_size <- current_block[adj_col]
+          ncell <- current_size * current_block[alt_col]
+        } else {
+          # Add current block to results and start new block
+          result_blocks <- rbind(result_blocks, current_block)
+          current_block <- row_blocks[i, ]
+          current_size <- current_block[adj_col]
+          ncell <- current_size * current_block[alt_col]
+        }
+      }
+      # Add final block
+      rbind(result_blocks, current_block)
+    })
+  )
+
+  rownames(aggregated_blocks) <- NULL
+  aggregated_blocks
+}
