a797e09f06790f07febe92e0f77cb879057a3352
Hugh Graham <hugh.a.graham@gmail.com>
2025-04-29 16:29:06 +0100

    tests for multiband composites

---
+++ tests/testthat/test-multiband_reduce.R
@@ -0,0 +1,101 @@
+test_that("multiband_reduce works", {
+  if (!vrtility:::using_daemons()) {
+    mirai::daemons(2)
+  }
+  s2files <- fs::dir_ls(system.file("s2-data", package = "vrtility"))
+
+  ex_collect <- vrt_collect(s2files)
+  t_block <- ex_collect[[1]][[1]]
+
+  ex_collect_mask <- ex_collect |>
+    vrt_set_maskfun(
+      mask_band = "SCL",
+      mask_values = c(0, 1, 2, 3, 8, 9, 10, 11),
+      drop_mask_band = TRUE
+    )
+  ex_collect_mask_warp <- ex_collect_mask |>
+    vrt_warp(
+      t_srs = t_block$srs,
+      te = t_block$bbox,
+      tr = t_block$res
+    )
+
+  # Geomedian weizfeld:
+  ex_geomed_weizfeld <- multiband_reduce(
+    ex_collect_mask_warp,
+    reduce_fun = geomedian()
+  )
+
+  expect_true(all(file.exists(ex_geomed_weizfeld)))
+  ds <- new(gdalraster::GDALRaster, ex_geomed_weizfeld)
+  vals <- gdalraster::read_ds(ds)
+  expect_equal(sum(vals, na.rm = TRUE), 635224136)
+  ds$close()
+
+  # Geomedian weizfeld impute_na = FALSE:
+  ex_geomed_weizfeld_na <- multiband_reduce(
+    ex_collect_mask_warp,
+    reduce_fun = geomedian(weizfeld = TRUE, impute_na = FALSE)
+  )
+
+  expect_true(all(file.exists(ex_geomed_weizfeld_na)))
+  ds <- new(gdalraster::GDALRaster, ex_geomed_weizfeld_na)
+  vals <- gdalraster::read_ds(ds)
+  expect_equal(sum(vals, na.rm = TRUE), 635224136)
+  ds$close()
+
+  # Geomedian Gmedian:
+  ex_geomed_gmedian <- multiband_reduce(
+    ex_collect_mask_warp,
+    reduce_fun = geomedian(weizfeld = FALSE)
+  )
+  expect_true(all(file.exists(ex_geomed_gmedian)))
+  ds <- new(gdalraster::GDALRaster, ex_geomed_gmedian)
+  vals <- gdalraster::read_ds(ds)
+  expect_equal(sum(vals, na.rm = TRUE), 633717594)
+  ds$close()
+
+  # medoid
+  ex_medoid <- multiband_reduce(
+    ex_collect_mask_warp,
+    reduce_fun = medoid()
+  )
+  expect_true(all(file.exists(ex_medoid)))
+  ds <- new(gdalraster::GDALRaster, ex_medoid)
+  vals <- gdalraster::read_ds(ds)
+  expect_equal(sum(vals, na.rm = TRUE), 624520710)
+  ds$close()
+
+  # medoid impute_na = FALSE:
+  ex_medoid_na <- multiband_reduce(
+    ex_collect_mask_warp,
+    reduce_fun = medoid(impute_na = FALSE)
+  )
+
+  expect_true(all(file.exists(ex_medoid_na)))
+  ds <- new(gdalraster::GDALRaster, ex_medoid_na)
+  vals <- gdalraster::read_ds(ds)
+  expect_equal(sum(vals, na.rm = TRUE), 624520710)
+  ds$close()
+
+  # quantoid
+  ex_quantoid <- multiband_reduce(
+    ex_collect_mask_warp,
+    reduce_fun = quantoid(probability = 0.1)
+  )
+  expect_true(all(file.exists(ex_quantoid)))
+  ds <- new(gdalraster::GDALRaster, ex_quantoid)
+  vals <- gdalraster::read_ds(ds)
+  expect_equal(sum(vals, na.rm = TRUE), 591765801)
+  ds$close()
+  # quantoid impute_na = FALSE:
+  ex_quantoid_na <- multiband_reduce(
+    ex_collect_mask_warp,
+    reduce_fun = quantoid("maximum", probability = 0.1, impute_na = FALSE)
+  )
+  expect_true(all(file.exists(ex_quantoid_na)))
+  ds <- new(gdalraster::GDALRaster, ex_quantoid_na)
+  vals <- gdalraster::read_ds(ds)
+  expect_equal(sum(vals, na.rm = TRUE), 591796630)
+  ds$close()
+})
