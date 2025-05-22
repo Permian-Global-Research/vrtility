84300de421021c9c3d8631793a488b446e8acabf
Hugh Graham <hugh.a.graham@gmail.com>
2025-05-22 13:40:08 +0100

    add a quantile pixel function and tests

--- R/zvrt-pixel-funs-composite.R
+++ R/zvrt-pixel-funs-composite.R
@@ -62,15 +62,46 @@ def pixfun(in_ar, out_ar, xoff, yoff, xsize, ysize, raster_xsize, raster_ysize, 
     no_data_val = int(kwargs['no_data_value'])
     # Stack the input arrays first
     stacked = np.stack(in_ar)
-    
+
     # Create a mask of no_data values
     mask = (stacked == no_data_val)
-    
+
     # Create a masked array - more efficient than np.where for this operation
     masked_data = np.ma.array(stacked, mask=mask, shrink=False)
-    
+
     # Calculate mean on masked array directly
     out_ar[:] = np.ma.exp(np.ma.mean(np.ma.log(masked_data), axis=0)).filled(fill_value=no_data_val)
+"
+  )
+}
+
+#' @param q Probability of the quantile to compute. Values must be between 0 and 1
+#' inclusive.
+#' @details `quantile_numpy` is a pixel function that calculates the quantile
+#' of the input arrays for a given probability. This could be useful where
+#' the median fails to filter cloudy or shadowy images effectively.
+#' @rdname vrt_set_py_pixelfun
+#' @export
+quantile_numpy <- function(q) {
+  v_assert_type(q, "q", "numeric", nullok = FALSE)
+  v_assert_within_range(q, "q", 0, 1)
+
+  glue::glue(
+    "
+import numpy as np
+import warnings
+def pixfun(in_ar, out_ar, xoff, yoff, xsize, ysize, raster_xsize, raster_ysize, buf_radius, gt, **kwargs):
+    no_data_val = int(kwargs['no_data_value'])
+    # Stack the input arrays first
+    stacked = np.stack(in_ar)
+
+    # Set nodata values to np.nan
+    stacked = np.where(stacked == no_data_val, np.nan, stacked)
+    
+    with warnings.catch_warnings():
+      warnings.simplefilter('ignore', category=RuntimeWarning)
+      out_ar[:] = np.nanquantile(stacked, {q}, axis=0)
+      out_ar[np.isnan(out_ar)] = no_data_val
 "
   )
 }
