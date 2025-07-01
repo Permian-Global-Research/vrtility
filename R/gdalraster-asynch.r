6b156f6a63b4002334ba305342a6058bf7c280a1
Hugh Graham <hugh.a.graham@gmail.com>
2025-07-01 19:56:24 +0100

    asynch and synch reading/writing modularized

--- R/gdalraster-asynch.r
+++ R/gdalraster-asynch.r
@@ -1,44 +1,65 @@
-async_gdalreader <- function(blocks, vrt_file, ds) {
+async_gdalreader_band_read_write <- function(blocks, vrt_file, ds) {
   # Create mirai map with promises
   jobs <- mirai::mirai_map(
     blocks,
     function(...) {
       # Extract block parameters correctly from the 1-row dataframe
       block_params <- rlang::dots_list(...)
-      inds <- methods::new(
-        gdalraster::GDALRaster,
-        vrt_file
-      )
+      inds <- methods::new(gdalraster::GDALRaster, vrt_file)
       on.exit(inds$close())
-      # # Read and combine bands
-      band_data <- compute_with_py_env(
-        inds$read(
-          band = block_params[["band_n"]],
-          xoff = block_params[["nXOff"]],
-          yoff = block_params[["nYOff"]],
-          xsize = block_params[["nXSize"]],
-          ysize = block_params[["nYSize"]],
-          out_xsize = block_params[["nXSize"]],
-          out_ysize = block_params[["nYSize"]]
-        )
-      )
-
-      bscale <- inds$getScale(block_params[["band_n"]])
-      if (!is.na(bscale)) {
-        band_data <- band_data * bscale
-      }
-
+      band_data <- blockreader(inds, block_params)
+      band_data <- scale_applicator(inds, band_data, block_params)
       return(band_data)
     },
     vrt_file = vrt_file,
-    block_params = blocks,
-    compute_with_py_env = compute_with_py_env
+    blockreader = blockreader,
+    scale_applicator = scale_applicator
   )
 
-  resolved <- logical(length(jobs))
+  mirai_async_result_handler(
+    jobs,
+    ds = ds,
+    blocks = blocks,
+    expr = rlang::expr(
+      blockwriter(ds, blocks[i, ], j) # nolint
+    ),
+    msg = "mirai GDAL read/write error"
+  )
+}
 
-  while (mirai::unresolved(jobs)) {
-    for (i in seq_along(jobs)) {
+
+sequential_gdalreader_band_read_write <- function(blocks, vrt_file, ds, quiet) {
+  purrr::pwalk(
+    blocks,
+    function(...) {
+      # Extract block parameters correctly from the 1-row dataframe
+      block_params <- rlang::dots_list(...)
+      inds <- methods::new(gdalraster::GDALRaster, vrt_file)
+      on.exit(inds$close())
+      # Read and combine bands
+      band_data <- blockreader(inds, block_params)
+
+      j_data <- scale_applicator(inds, band_data, block_params)
+
+      blockwriter(ds, block_params, j_data)
+    },
+    .progress = !quiet
+  )
+}
+
+mirai_async_result_handler <- function(
+  jobs,
+  ds,
+  ...,
+  expr,
+  msg = "mirai async error"
+) {
+  dots <- rlang::list2(...)
+  rlang::env_bind(rlang::current_env(), !!!dots)
+  resolved <- logical(length(jobs))
+  unresolved_idx <- seq_along(jobs)
+  while (any(!resolved)) {
+    for (i in unresolved_idx) {
       if (inherits(jobs[[i]]$data, "unresolvedValue")) {
         next
       }
@@ -47,27 +68,173 @@ async_gdalreader <- function(blocks, vrt_file, ds) {
         next
       }
 
-      j <- jobs[[i]][]
+      j <- mirai::collect_mirai(jobs[[i]])
 
       if (inherits(j, "character")) {
         cli::cli_abort(
-          "mirai GDAL read error:  {j}",
-          class = "mirai_gdal_read_error"
+          "{msg}:  {j}",
+          class = "mirai_async_error"
         )
       }
 
-      bps <- blocks[i, ]
-
-      ds$write(
-        band = bps[["band_n"]],
-        xoff = bps[["nXOff"]],
-        yoff = bps[["nYOff"]],
-        xsize = bps[["nXSize"]],
-        ysize = bps[["nYSize"]],
-        rasterData = j
-      )
+      rlang::eval_bare(expr)
       # If the job is resolved, mark it as such
       resolved[[i]] <- TRUE
+      unresolved_idx <- which(!resolved)
     }
   }
+}
+
+blockreader <- function(inds, block_params) {
+  compute_with_py_env(
+    inds$read(
+      band = block_params[["band_n"]],
+      xoff = block_params[["nXOff"]],
+      yoff = block_params[["nYOff"]],
+      xsize = block_params[["nXSize"]],
+      ysize = block_params[["nYSize"]],
+      out_xsize = block_params[["nXSize"]],
+      out_ysize = block_params[["nYSize"]]
+    )
+  )
+}
+
+blockwriter <- function(ds, bps, data) {
+  ds$write(
+    band = bps[["band_n"]],
+    xoff = bps[["nXOff"]],
+    yoff = bps[["nYOff"]],
+    xsize = bps[["nXSize"]],
+    ysize = bps[["nYSize"]],
+    rasterData = data
+  )
+  ds$flushCache() # I think we want this to clear  memory as we go...
+}
+
+scale_applicator <- function(inds, band_data, block_params) {
+  bscale <- inds$getScale(block_params[["band_n"]])
+  if (!is.na(bscale)) {
+    band_data <- band_data * bscale
+  }
+  return(band_data)
+}
+
+
+async_gdalreader_multiband_reduce_read_write <- function(
+  blocks,
+  vrt_file,
+  ds,
+  raster_template,
+  reduce_fun,
+  apply_scale
+) {
+  jobs <- mirai::mirai_map(
+    blocks,
+    function(...) {
+      # Extract block parameters correctly from the 1-row dataframe
+      block_params <- rlang::dots_list(...)
+      ras_band_dat <- read_block_arrays(
+        vrt_file,
+        nbands = nbands,
+        xoff = block_params[["nXOff"]],
+        yoff = block_params[["nYOff"]],
+        xsize = block_params[["nXSize"]],
+        ysize = block_params[["nYSize"]]
+      )
+      cell_vals <- mdim_reduction_apply(ras_band_dat, reduce_fun)
+      list(
+        data = restructure_cells(cell_vals),
+        block = block_params
+      )
+    },
+    vrt_file = vrt_file,
+    nbands = raster_template$nbands,
+    read_block_arrays = read_block_arrays,
+    reduce_fun = reduce_fun,
+    mdim_reduction_apply = mdim_reduction_apply,
+    restructure_cells = restructure_cells
+  )
+
+  mirai_async_result_handler(
+    jobs,
+    ds = ds,
+    blocks = blocks,
+    raster_template = raster_template,
+    apply_scale = apply_scale,
+    expr = rlang::expr(
+      multiband_writer(j, ds, raster_template, apply_scale) # nolint
+    ),
+    msg = "mirai GDAL multi-band reducer error"
+  )
+}
+
+
+sequential_gdalreader_multiband_reduce_read_write <- function(
+  blocks,
+  vrt_file,
+  ds,
+  raster_template,
+  reduce_fun,
+  apply_scale,
+  quiet
+) {
+  jobs <- purrr::pmap(
+    blocks,
+    function(...) {
+      # Extract block parameters correctly from the 1-row dataframe
+      block_params <- rlang::dots_list(...)
+      ras_band_dat <- read_block_arrays(
+        vrt_file,
+        nbands = raster_template$nbands,
+        xoff = block_params[["nXOff"]],
+        yoff = block_params[["nYOff"]],
+        xsize = block_params[["nXSize"]],
+        ysize = block_params[["nYSize"]]
+      )
+      cell_vals <- mdim_reduction_apply(ras_band_dat, reduce_fun)
+      list(
+        data = restructure_cells(cell_vals),
+        block = block_params
+      )
+    },
+    .progress = !quiet
+  )
+
+  purrr::walk(jobs, function(j) {
+    multiband_writer(j, ds, raster_template, apply_scale)
+  })
+}
+
+
+multiband_writer <- function(j, ds, raster_template, apply_scale) {
+  n_cells <- j$block$nXSize * j$block$nYSize
+  # Calculate start and end indices for each band
+  band_starts <- seq(1, n_cells * raster_template$nbands, by = n_cells)
+  band_ends <- band_starts + n_cells - 1
+
+  j$data[is.na(j$data)] <- raster_template$nodataval
+
+  purrr::walk(seq_len(raster_template$nbands), function(b) {
+    start_idx <- band_starts[b]
+    end_idx <- band_ends[b]
+
+    bscale <- raster_template$scale_vals[b]
+    bdata <- if (!is.na(bscale) && apply_scale) {
+      j$data[start_idx:end_idx] * bscale
+      # Get the data for this band
+      band_data <- j$data[start_idx:end_idx]
+
+      # Create mask for valid data (not equal to nodata)
+      valid_mask <- band_data != raster_template$nodataval
+
+      # Apply scale only to valid data
+      band_data[valid_mask] <- band_data[valid_mask] * bscale
+
+      band_data
+    } else {
+      j$data[start_idx:end_idx]
+    }
+
+    blockwriter(ds, c(band_n = b, j$block), bdata)
+  })
 }
