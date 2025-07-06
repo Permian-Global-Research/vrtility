14472129fa5185e83f9989bbe9867409acfabc9d
Hugh Graham <hugh.a.graham@gmail.com>
2025-07-06 22:16:02 +0100

    improved vrt_set_scale which now works as intended for complex sources

--- R/vrt-set-scale.R
+++ R/vrt-set-scale.R
@@ -8,6 +8,12 @@
 #' object. Default is 0.
 #' @param band_idx  numeric; the target band position(s) to set the scale value
 #' for. If NULL, The scale_value will be set for all bands.
+#' @details
+#' This sets the Scale and Offset XML tags for the VRT_x object. This is
+#' useful for scaling the pixel values of the VRT_x object. This is however,
+#' a little confusing... If your sources are ComplexSources, then this function
+#' applies the scale and offset to the ComplexSource elements in addition to the
+#' raster bands. If the sources are SimpleSources (which don't support)
 #' @examples
 #' s2files <- fs::dir_ls(system.file("s2-data", package = "vrtility"))
 #' ex_collect <- vrt_collect(s2files)
@@ -38,50 +44,52 @@ vrt_set_scale.vrt_block <- function(
   offset_value = 0.0,
   band_idx = NULL
 ) {
-  v_assert_type(scale_value, "scale_value", "numeric", nullok = FALSE)
-  v_assert_type(
-    band_idx,
-    "band_idx",
-    c("numeric", "integer"),
-    nullok = TRUE,
-    multiple = TRUE
+  purrr::pwalk(
+    list(
+      v = list(scale_value, offset_value),
+      n = list("scale_value", "offset_value")
+    ),
+    function(v, n) {
+      if (length(v) > 1) {
+        v_assert_length(
+          v,
+          n,
+          length(x$assets),
+          nullok = FALSE
+        )
+      }
+    }
   )
-
-  if (length(scale_value) > 1) {
-    v_assert_length(
-      scale_value,
-      "scale_value",
-      length(x$assets),
-      nullok = FALSE
-    )
-  }
 
   # Get the VRT XML
   vrt_xml <- xml2::read_xml(x$vrt)
 
-  # Insert the new band after the specified position
   bands <- xml2::xml_find_all(vrt_xml, ".//VRTRasterBand")
 
   if (is.null(band_idx)) {
     band_idx <- seq_along(bands)
   }
 
-  purrr::walk2(bands[band_idx], scale_value, function(band, scale) {
-    # Remove existing Scale elements
-    xml2::xml_remove(xml2::xml_find_all(band, ".//Scale"))
-    xml2::xml_add_child(
-      band,
-      "Scale",
-      format(scale, scientific = FALSE)
-    )
-    # Remove existing Offset elements
-    xml2::xml_remove(xml2::xml_find_all(band, ".//Offset"))
-    xml2::xml_add_child(
-      band,
-      "Offset",
-      format(offset_value, scientific = FALSE)
-    )
-  })
+  purrr::pwalk(
+    list(band = bands[band_idx], scale = scale_value, offset = offset_value),
+    function(band, scale, offset) {
+      # Remove existing Scale elements
+      reset_element(band, "Scale", scale)
+
+      # Remove existing Offset elements
+      reset_element(band, "Offset", offset)
+
+      band_src <- xml2::xml_find_first(
+        band,
+        ".//ComplexSource"
+      )
+      # Remove existing ScaleOffset elements
+      reset_element(band_src, "ScaleOffset", offset)
+      reset_element(band_src, "ScaleRatio", scale)
+
+      xml2::xml_set_attr(band, "dataType", "Float32")
+    }
+  )
   out_vrt <- fs::file_temp(
     tmp_dir = getOption("vrt.cache"),
     ext = "vrt"
