c3bcfd62f613d1be336936529e7057b6bb335ce0
Hugh Graham <hugh.a.graham@gmail.com>
2025-09-29 13:36:37 +0100

    can't get promise eval to work on windows - so use promises for unix and polling for windows

--- R/mirai-async-result-handler.R
+++ R/mirai-async-result-handler.R
@@ -1,4 +1,4 @@
-#' @title Mirai asynchronous result handler
+#' @title Mirai asynchronous result handler for unix
 #' @description Handles the results of asynchronous mirai jobs, checking for
 #' unresolved values and evaluating an expression when resolved.
 #' @param jobs A list of mirai jobs.
@@ -9,7 +9,7 @@
 #' @return invisible
 #' @keywords internal
 #' @noRd
-mirai_async_result_handler <- function(
+mirai_async_result_handler_unix <- function(
   jobs,
   ds,
   ...,
@@ -50,17 +50,9 @@ mirai_async_result_handler <- function(
   close_datasets_safely(promise_list, ds)
 
   # "Pump" the event loop to process promise callbacks
-
-  if (.Platform$OS.type == "windows") {
-    run_now_dur <- 0.1
-    sleep_interval <- 0.01
-    max_sleep_interval <- 0.3
-  } else {
-    run_now_dur <- 0.01
-    sleep_interval <- 0.001
-    max_sleep_interval <- 0.1
-  }
-
+  run_now_dur <- 0.01
+  sleep_interval <- 0.001
+  max_sleep_interval <- 0.1
   while (completed_count < length(jobs)) {
     later::run_now(run_now_dur) # This processes promise callbacks!
     Sys.sleep(sleep_interval)
@@ -102,4 +94,83 @@ close_datasets_safely <- function(plist, ds) {
     )
 
   return(invisible())
+}
+
+
+#' @title Mirai asynchronous result handler for windows
+#' @description Handles the results of asynchronous mirai jobs, does not use
+#' promises. using promises fails tests on windows...
+#' @param jobs A list of mirai jobs.
+#' @param ds The GDALRaster dataset to write to.
+#' @param ... Additional arguments to bind in the current environment.
+#' @param expr The expression to evaluate when a job is resolved.
+#' @param msg The message to display in case of an error.
+#' @return invisible
+#' @keywords internal
+#' @noRd
+mirai_async_result_handler_win <- function(
+  jobs,
+  ds,
+  ...,
+  expr,
+  msg = "mirai async error"
+) {
+  dots <- rlang::list2(...)
+  rlang::env_bind(rlang::current_env(), !!!dots)
+  resolved <- logical(length(jobs))
+  unresolved_idx <- seq_along(jobs)
+  while (any(!resolved)) {
+    for (i in unresolved_idx) {
+      if (inherits(jobs[[i]]$data, "unresolvedValue")) {
+        next
+      }
+
+      if (resolved[[i]]) {
+        next
+      }
+
+      j <- mirai::collect_mirai(jobs[[i]])
+
+      if (inherits(j, "miraiError")) {
+        cli::cli_abort(
+          "{msg}:  {j}",
+          class = "mirai_async_error"
+        )
+      }
+      browser()
+
+      rlang::eval_bare(expr)
+      # If the job is resolved, mark it as such
+      resolved[[i]] <- TRUE
+      unresolved_idx <- which(!resolved)
+    }
+  }
+  return(invisible())
+}
+
+
+mirai_async_result_handler <- function(
+  jobs,
+  ds,
+  ...,
+  expr,
+  msg = "mirai async error"
+) {
+  if (.Platform$OS.type == "windows") {
+    mirai_async_result_handler_win(
+      jobs = jobs,
+      ds = ds,
+      ...,
+      expr = expr,
+      msg = msg
+    )
+  } else {
+    mirai_async_result_handler_unix(
+      jobs = jobs,
+      ds = ds,
+      ...,
+      expr = expr,
+      msg = msg
+    )
+  }
 }
