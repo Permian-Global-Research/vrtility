cd8e4a58a641f4c9326b2b94dca255884bfbafb7
Hugh Graham <hugh.a.graham@gmail.com>
2025-10-16 01:58:22 +0100

    hls assertions

--- R/assertions.R
+++ R/assertions.R
@@ -259,3 +259,24 @@ v_assert_muparser <- function() {
     ))
   }
 }
+
+
+v_asset_hls_catalog <- function(stac_source, collection) {
+  if (stac_source == "https://planetarycomputer.microsoft.com/api/stac/v1/") {
+    collection <- if (!collection %in% c("hls2-s30", "hls2-l30")) {
+      cli::cli_abort(c(
+        "When using the Planetary Computer STAC source,",
+        "the HLS collection must be specified as one of 
+        {.val hls2-s30} or {.val hls2-l30}"
+      ))
+    }
+  } else {
+    collection <- if (!collection %in% c("HLSS30_2.0", "HLSL30_2.0")) {
+      cli::cli_abort(c(
+        "When using the NASA EARTHDATA STAC source,",
+        "the HLS collection must be specified as one of 
+        {.val HLSS30_2.0} or {.val HLSL30_2.0}"
+      ))
+    }
+  }
+}
