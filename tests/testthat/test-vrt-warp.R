8a0190adc50af86301affc45422a359536d9b653
Hugh Graham <hugh.a.graham@gmail.com>
2025-11-25 22:37:41 +0000

    tests for new warper

---
+++ tests/testthat/test-vrt-warp.R
@@ -0,0 +1,69 @@
+vrt_warp_tests <- function() {
+  s2files <- fs::dir_ls(system.file("s2-data", package = "vrtility"))
+
+  ex_collect <- vrt_collect(
+    s2files,
+    datetimes = c(
+      # these dates are made up btw.
+      "2024-01-01",
+      "2024-03-01",
+      "2024-05-01",
+      "2024-07-01",
+      "2024-09-01"
+    )
+  )
+
+  t_block <- ex_collect[[1]][[1]]
+
+  ex_collect_warp <- ex_collect |>
+    vrt_warp(
+      t_srs = t_block$srs,
+      te = t_block$bbox,
+      tr = t_block$res,
+      quiet = FALSE,
+      lazy = TRUE
+    )
+
+  ds <- methods::new(
+    gdalraster::GDALRaster,
+    ex_collect_warp$vrt[[1]]$vrt_src
+  )
+  expect_true(all(fs::path_ext(ds$getFileList()) == "vrt"))
+  expect_true(all(ds$getBlockSize(1) == c(361, 128)))
+
+  ds$close()
+
+  ex_collect_warp <- ex_collect |>
+    vrt_warp(
+      t_srs = t_block$srs,
+      te = t_block$bbox,
+      tr = t_block$res,
+      quiet = FALSE,
+      lazy = FALSE
+    )
+
+  ds <- methods::new(
+    gdalraster::GDALRaster,
+    ex_collect_warp$vrt[[1]]$vrt_src
+  )
+  expect_true(all(
+    fs::path_ext(ds$getFileList()[2:length(ds$getFileList())]) == "tif"
+  ))
+  expect_true(all(ds$getBlockSize(1) == c(256, 256)))
+
+  ds$close()
+}
+
+test_that("vrt_warp works async", {
+  if (!mirai::daemons_set()) {
+    mirai::daemons(2)
+  }
+  vrt_warp_tests()
+})
+
+test_that("vrt_warp works synchronously", {
+  if (mirai::daemons_set()) {
+    mirai::daemons(0)
+  }
+  vrt_warp_tests()
+})
