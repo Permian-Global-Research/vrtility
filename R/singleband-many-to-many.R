51ae6f8845f04cc19e67b1c4fefcabedb3ee9c01
Hugh Graham <hugh.a.graham@gmail.com>
2025-12-12 15:12:17 +0000

    drop insistent function call for single_band_reader also add note about possible enhancement

--- R/singleband-many-to-many.R
+++ R/singleband-many-to-many.R
@@ -331,18 +331,13 @@ hampel_filter <- function(k = 1L, t0 = 3, impute_na = FALSE) {
 #' within singleband_m2m() when used in parallel.
 #' @noRd
 #' @keywords internal
-single_band_reader <- function() {
-  purrr::insistently(
-    function(blk, block_params, config_options) {
-      vrtfile <- blk$vrt_src
-      inds <- methods::new(gdalraster::GDALRaster, vrtfile)
-      on.exit(inds$close(), add = TRUE)
-      blockreader(inds, block_params, config_options)
-    },
-    rate = purrr::rate_backoff(
-      pause_base = getOption("vrt.pause.base"),
-      pause_cap = getOption("vrt.pause.cap"),
-      max_times = getOption("vrt.max.times")
-    )
-  )
+single_band_reader <- function(blk, block_params, config_options) {
+  vrtfile <- blk$vrt_src
+  #TODO: would the following be better for mirai?
+  # if (!exists("inds")) {
+  #   inds <<- methods::new(gdalraster::GDALRaster, vrt_file) # nolint
+  # }
+  inds <- methods::new(gdalraster::GDALRaster, vrtfile)
+  on.exit(inds$close(), add = TRUE)
+  blockreader(inds, block_params, config_options)
 }
